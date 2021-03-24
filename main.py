10a9cc1d351efdeac9785b86db08eac90a490ec2
Maksim787 <72660795+Maksim787@users.noreply.github.com>
2021-03-24 13:15:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import get_info
 parse_files.main()
 merge_files.main()
 df = get_info.main()
+for col in df.columns:
+    if max(df[col]) == 0:
+        del df[col]
 print(df)
+print("-" * 150)
+
+df_sorted = df.copy()
+df_sorted["Sort_factor"] = (df["KR_total"] + 0.3) / df["DZ_total"]
+df_sorted = df_sorted.sort_values(by=["Sort_factor", "DZ_total"], ascending=[True, False])
+print(df_sorted)
