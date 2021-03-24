f62025f940f951b55d6ebe86e40876223fdb3e2d
Maksim787 <72660795+Maksim787@users.noreply.github.com>
2021-03-24 15:08:37 +0300

    Add files via upload

--- YandexContest/get_info.py
+++ YandexContest/get_info.py
@@ -10,12 +10,17 @@ def main():
     df = pd.read_csv("all.csv")
     df["DZ_total"] = 0.05 * sum([df["DZ_" + str(i)] for i in range(1, 9)]) / 10
     df["KR_total"] = 0.1 * sum([df["KR_" + str(i)] for i in range(1, 5)]) / 5
+
+    df["DZ_total"] = df["DZ_total"].round(5)
+    df["KR_total"] = df["KR_total"].round(5)
+
     df["Mark"] = df["DZ_total"] + df["KR_total"]
     df = df.sort_values(by="Mark", ascending=False)
     df.index = pd.Series(range(1, df.shape[0] + 1))
     df["Percentile"] = (df.index - 1) * 100 / df.shape[0]
     df["Percentile"] = df["Percentile"].round(1)
     df["Percent of done works"] = df["Mark"] / (0.05 * 8 + 0.1 * 4) * 10
+    df["Percent of done works"] = df["Percent of done works"].round(5)
     for col in df.columns:
         if max(df[col]) == 0:
             del df[col]
